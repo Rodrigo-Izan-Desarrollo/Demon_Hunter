b0acedd86c15498d792145a24c1a1acb983d7666
izansl <125253405+izansl@users.noreply.github.com>
2023-10-11 13:34:38 +0200

    Cambios en player
    -Incluido Animation.h
    -Añadidas diversas animaciones al player
          -Con sus respectivas cordenadas
    -Añadido nuevo sprite set para player
    -Cambiado en el config.xml la ruta del sprite para el nuevo

--- Game/Source/Player.cpp
+++ Game/Source/Player.cpp
@@ -21,6 +21,59 @@ bool Player::Awake() {
 
 	//L03: DONE 2: Initialize Player parameters
 	position = iPoint(config.attribute("x").as_int(), config.attribute("y").as_int());
+	player_1.PushBack({0,2,32,32});
+	player_1.PushBack({ 31,2,32,32 });
+	player_1.speed = 0.5f;
+
+	player_speed_1.PushBack({0 ,100, 32, 32});
+	player_speed_1.PushBack({ 31 ,100, 32, 32 });
+	player_speed_1.PushBack({ 63 ,100, 32, 32 });
+	player_speed_1.PushBack({ 95 ,100, 32, 32 });
+	player_speed_1.PushBack({ 126 ,100, 32, 32 });
+	player_speed_1.PushBack({ 159 ,100, 32, 32 });
+	player_speed_1.PushBack({ 191 ,100, 32, 32 });
+	player_speed_1.PushBack({ 222 ,100, 32, 32 });
+
+	player_walk_1.PushBack({0, 67, 32, 32});
+	player_walk_1.PushBack({ 31, 67, 32, 32 });
+	player_walk_1.PushBack({ 63, 67, 32, 32 });
+	player_walk_1.PushBack({ 95, 67, 32, 32 });
+
+	player_down_1.PushBack({0, 131, 32, 32});
+	player_down_1.PushBack({ 31, 131, 32, 32 });
+	player_down_1.PushBack({ 63, 131, 32, 32 });
+	player_down_1.PushBack({ 126, 131, 32, 32 });
+
+	player_jump_1.PushBack({0, 163, 32, 32});
+	player_jump_1.PushBack({ 31, 163, 32, 32 });
+	player_jump_1.PushBack({ 63, 163, 32, 32 });
+	player_jump_1.PushBack({ 95, 163, 32, 32 });
+	player_jump_1.PushBack({ 126, 163, 32, 32 });
+	player_jump_1.PushBack({ 159, 163, 32, 32 });
+	player_jump_1.PushBack({ 191, 163, 32, 32 });
+	player_jump_1.PushBack({ 222, 163, 32, 32 });
+
+	player_atack_1.PushBack({0,259, 32, 32});
+	player_atack_1.PushBack({ 31,259, 32, 32 });
+	player_atack_1.PushBack({ 63,259, 32, 32 });
+	player_atack_1.PushBack({ 95,259, 32, 32 });
+	player_atack_1.PushBack({ 126,259, 32, 32 });
+	player_atack_1.PushBack({ 159,259, 32, 32 });
+	player_atack_1.PushBack({ 191,259, 32, 32 });
+	player_atack_1.PushBack({ 222,259, 32, 32 });
+
+	player_hurt_1.PushBack({0,227, 32, 32});
+	player_hurt_1.PushBack({ 31,227, 32, 32 });
+
+	player_dead_1.PushBack({63, 227, 32, 32});
+	player_dead_1.PushBack({ 95, 227, 32, 32 });
+	player_dead_1.PushBack({ 126, 227, 32, 32 });
+	player_dead_1.PushBack({ 159, 227, 32, 32 });
+
+	player_no_1.PushBack({0, 195, 32, 32});
+	player_no_1.PushBack({ 31, 195, 32, 32 });
+	player_no_1.PushBack({ 63, 195, 32, 32 });
+	player_no_1.PushBack({ 95, 195, 32, 32 });
 
 	return true;
 }
@@ -28,6 +81,8 @@ bool Player::Awake() {
 bool Player::Start() {
 
 	texture = app->tex->Load(config.attribute("texturePath").as_string());
+	currentAnimation= &player_1;
+	
 	return true;
 }
 
@@ -47,7 +102,7 @@ bool Player::Update(float dt)
 	if (app->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT)
 		position.x += (int)(speed * dt);
 
-	app->render->DrawTexture(texture,position.x,position.y);
+	app->render->DrawTexture(texture,position.x,position.y,&player_1.GetCurrentFrame());
 
 	return true;
 }
--- Game/Source/Player.h
+++ Game/Source/Player.h
@@ -4,6 +4,7 @@
 #include "Entity.h"
 #include "Point.h"
 #include "SDL/include/SDL.h"
+#include "Animation.h"
 
 struct SDL_Texture;
 
@@ -28,6 +29,25 @@ public:
 	//L02: DONE 2: Declare player parameters
 	float speed = 0.2f;
 	SDL_Texture* texture = NULL;
+	Animation* currentAnimation;
+	Animation player_1;
+	Animation player_walk_1;
+	Animation player_speed_1;
+	Animation player_down_1;
+	Animation player_jump_1;
+	Animation player_no_1;
+	Animation player_hurt_1;
+	Animation player_dead_1;
+	Animation player_atack_1;
+	Animation player_2;
+	Animation player_walk_2;
+	Animation player_speed_2;
+	Animation player_down_2;
+	Animation player_jump_2;
+	Animation player_no_2;
+	Animation player_hurt_2;
+	Animation player_dead_2;
+	Animation player_atack_2;
 	pugi::xml_node config;
 };
 
