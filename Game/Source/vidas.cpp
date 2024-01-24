5259a9e55c7deaa1ad217aa38028425c7e05efbc
Rodrigomoize <rodrimoize@gmail.com>
2024-01-24 12:24:25 +0100

    f

---
+++ Game/Source/vidas.cpp
@@ -0,0 +1,86 @@
+#include "Powerup_1.h"
+#include "App.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "Input.h"
+#include "Render.h"
+#include "Scene.h"
+#include "Log.h"
+#include "Point.h"
+#include "Physics.h"
+
+Powerup_1::Powerup_1() : Entity(EntityType::POWERUP_1)
+{
+	name.Create("Powerup");
+}
+
+Powerup_1::~Powerup_1() {}
+
+bool Powerup_1::Awake() {
+
+	position.x = parameters.attribute("x").as_int();
+	position.y = parameters.attribute("y").as_int();
+	texturePath = parameters.attribute("texturepathpower_1").as_string();
+
+	return true;
+}
+
+bool Powerup_1::Start() {
+
+	//Initilize textures
+	texture = app->tex->Load(texturePath);
+
+	//Create pbody
+	pbody = app->physics->CreateCircle(position.x+16, position.y, 13, bodyType::STATIC);
+	pbody->ctype = ColliderType::POWERUP_1;
+	pbody->listener = this;
+
+	//Animation
+	idle.PushBack({ 0, 0, 32, 32 });
+	idle.PushBack({ 32, 0, 32, 32 });
+	idle.PushBack({ 64, 0, 32, 32 });
+	idle.loop = true;
+	idle.speed = 0.125f;
+
+	currentAnimation = &idle;
+
+	return true;
+}
+
+bool Powerup_1::Update(float dt)
+{ 
+	currentAnimation->Update();
+
+	//When collision with player destroy the itslef
+	if (isPicked)
+	{
+		SDL_DestroyTexture(texture);
+		pbody->body->SetActive(false);
+		app->entityManager->DestroyEntity(this);
+		app->physics->world->DestroyBody(pbody->body);
+	}
+
+	position.x = METERS_TO_PIXELS(pbody->body->GetTransform().p.x) - 16;
+	position.y = METERS_TO_PIXELS(pbody->body->GetTransform().p.y) - 16;
+
+	app->render->DrawTexture(texture, position.x, position.y, &currentAnimation->GetCurrentFrame());
+
+	return true;
+}
+
+bool Powerup_1::CleanUp()
+{
+	return true;
+}
+
+void Powerup_1::OnCollision(PhysBody* physA, PhysBody* physB) {
+
+	switch (physB->ctype)
+	{
+	case ColliderType::PLAYER:
+		LOG("Collision ITEM");
+		isPicked = true;
+		break;
+	}
+
+}
---
+++ Game/Source/vidas.h
@@ -0,0 +1,44 @@
+#ifndef __POWERUP1_H__
+#define __POWERUP1_H__
+
+#include "Entity.h"
+#include "Point.h"
+#include "SDL/include/SDL.h"
+#include "Animation.h"
+#include "Timer.h"
+
+struct SDL_Texture;
+
+class Powerup_1 : public Entity
+{
+public:
+
+	Powerup_1();
+	virtual ~Powerup_1();
+
+	bool Awake();
+
+	bool Start();
+
+	bool Update(float dt);
+
+	bool CleanUp();
+
+	void OnCollision(PhysBody* physA, PhysBody* physB);
+
+public:
+
+	Animation* currentAnimation;
+
+	Animation idle;
+
+	bool isPicked = false;
+
+	SDL_Texture* texture;
+
+	const char* texturePath;
+
+	PhysBody* pbody;
+};
+
+#endif // __SLIME_H__
